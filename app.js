dd9841b8ec102cc92fdd26c5de67b2a08642955b
Fahad-Rafiq06 <73123765+Fahad-Rafiq06@users.noreply.github.com>
2022-09-10 20:28:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
 var container = document.getElementById('container');
+var heading = document.createElement("h1");
+heading.setAttribute("class", "one");
+var headText = document.createTextNode("Todo App")
+heading.append(headText)
+container.append(heading)
 // containerbox
 var one = document.createElement('div');
-one.setAttribute("class","one");
+one.setAttribute("class", "one");
 container.append(one);
 // box
- var add = document.createElement('input');
- add.setAttribute("placeholder","ADD_TEXT")
- add.style.height = "50px";
- add.style.width = "350px";
- add.style.backgroundColor = "white";
- add.style.border = "2px solid black";
- add.style.paddingLeft = "20px"
- add.style.borderRadius = "5px"
- one.append(add);
+var add = document.createElement('input');
+add.setAttribute("placeholder", "What to do?")
+add.style.height = "50px";
+add.style.width = "350px";
+add.style.backgroundColor = "white";
+add.style.border = "2px solid black";
+add.style.paddingLeft = "20px"
+add.style.borderRadius = "5px"
+one.append(add);
 //  button
- var btn = document.createElement('button');
- var text = document.createTextNode("Add")
- btn.appendChild(text);
- btn.setAttribute("onClick","put()")
- btn.style.height = "50px";
- btn.style.width = "100px";
- btn.style.borderRadius = "5px"
- btn.style.backgroundColor = "black";
- btn.style.border = "2px solid white";
- btn.style.color = "white";
- btn.style.marginLeft = "10px"
- one.append(btn)
+var btn = document.createElement('button');
+var text = document.createTextNode("Add")
+btn.appendChild(text);
+btn.setAttribute("onClick", "put()")
+btn.style.height = "50px";
+btn.style.width = "100px";
+btn.style.borderRadius = "5px"
+btn.style.backgroundColor = "black";
+btn.style.border = "2px solid white";
+btn.style.color = "white";
+btn.style.marginLeft = "10px"
+one.append(btn)
 
 //  listcontainer
- var two = document.createElement('div')
- two.setAttribute("class","two")
+var two = document.createElement('div')
+two.setAttribute("class", "two")
 container.append(two);
 
-
-
-
 // functionofadd
-function put()
-{
+function put() {
     v = add.value;
     console.log(v)
     add.value = "";
@@ -51,10 +52,10 @@ function put()
     editbtn.style.width = "50px";
     editbtn.style.borderRadius = "5px"
     editbtn.style.backgroundColor = "white";
-    editbtn.style.border = "1px solid black";
+    editbtn.style.border = "1px solid gold";
     editbtn.style.marginLeft = "40%"
 
-    // DELETBUTTON
+    // delete button
     var dltbtn = document.createElement("button");
     var d = document.createTextNode("DELETE")
     dltbtn.appendChild(d);
@@ -63,20 +64,47 @@ function put()
     dltbtn.style.width = "70px";
     dltbtn.style.borderRadius = "5px"
     dltbtn.style.backgroundColor = "white";
-    dltbtn.style.border = "1px solid black";
+    dltbtn.style.border = "1px solid gold";
     dltbtn.style.marginLeft = "5px";
 
     vl = document.createElement('div');
     vl.setAttribute("class", "show");
-    vl.setAttribute("id",Math.random())
+    vl.setAttribute("id", Math.random())
     t = document.createTextNode(v)
     vl.append(t)
     vl.appendChild(editbtn)
     vl.appendChild(dltbtn)
     two.append(vl)
-  
-}
-function Delete()
-{
 
 }
+function Delete() {
+    vl.remove()
+}
+
+function edit() {
+    // var editTask = function(){
+        console.log("Edit task...");
+    
+    
+        var listItem = this.parentNode;
+        var editInput = listItem.querySelector("vl");
+        var label = listItem.querySelector("label");
+    
+        var containsClass = listItem.classList.contains("editMode");
+    
+    
+            //if the class pf parent is .editmode
+        if (containsClass){
+          //label text become the input's value  
+          label.innerText = editInput.value;
+    
+        } else {
+            //switch to .editmode
+            //input value becomes the label's text
+          editInput.vaule = label.innerText;
+        }
+    
+        listItem.classList.toggle("editMode"); //toggle .editmode on the parent
+    
+    }
+// }
